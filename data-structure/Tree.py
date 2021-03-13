cc6f153d84e31d097cc225431053794cb0ea344e
momo <wdudtlrw78@gmail.com>
2021-03-13 16:04:14 +0900

    Updated Tree delete

--- data-structure/Tree.py
+++ data-structure/Tree.py
@@ -1,53 +1,140 @@
-# create node
-
 class Node:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-# insert
-
+        
 class NodeMgmt:
-  def __init__(self, head):
-    self.head = head
+    def __init__(self, head):
+        self.head = head
+    
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if value < self.current_node.value:
+                if self.current_node.left != None:
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else:
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = Node(value)
+                    break
+    
+    def search(self, value):
+        self.current_node = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+        return False        
+    
+    def delete(self, value):
+        # 삭제할 노드 탐색
+        searched = False
+        self.current_node = self.head
+        self.parent = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                searched = True
+                break
+            elif value < self.current_node.value:
+                self.parent = self.current_node
+                self.current_node = self.current_node.left
+            else:
+                self.parent = self.current_node
+                self.current_node = self.current_node.right
 
-  def insert(self, value):
-    self.current_node = self.head
-    while True:
-      # 현재 노드 값 보다 작다면
-      if value < self.current_node.value:
-        # node left에 node가 있다면
-        if self.current_node.left != None:
-          self.current_node = self.current_node.left
-        else :
-          self.current_node_left = Node(value)
-          break
-      else:
-        if self.current_node.right != None:
-          self.current_node = self.current_node.right
-        else:
-          self.current_node.right = Node(value)
-          break
+        if searched == False:
+            return False    
 
-# search
-  def search(self, value):
-    self.current_node = self.head
-    while self.current_node:
-      if self.current_node.value == value:
+        # case1: 삭제할 Node가 Leaf Node인 경우
+        if  self.current_node.left == None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = None
+            else:
+                self.parent.right = None
+        
+        # case2: 삭제할 Node가 Child Node를 한 개 가지고 있을 경우
+        elif self.current_node.left != None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.left
+            else:
+                self.parent.right = self.current_node.left
+        elif self.current_node.left == None and self.current_node.right != None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.right
+            else:
+                self.parent.right = self.current_node.right        
+        
+        # case 3
+        elif self.current_node.left != None and self.current_node.right != None:
+            # case3-1 : 삭제할 Node가 두 개 경우 (Node가 Parent Node 왼쪽에 있을 때)
+            if value < self.parent.value:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent.left = self.change_node
+                self.change_node.right = self.current_node.right
+                self.change_node.left = self.change_node.left
+            # case 3-2: 삭제할 Node가 두 개 경우 (Node가 Parent Node 오른쪽에 있을 때)
+            else:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent.right = self.change_node
+                self.change_node.right = self.current_node.right
+                self.change_node.left = self.current_node.left
+
         return True
-      elif value < self.current_node.value:
-        self.current_node = self.current_node.left
-      else:
-        self.current_node = self.current_node.right
-    return False
 
-head = Node(1)
-BST = NodeMgmt(head)
-BST.insert(2)
-BST.insert(3)
-BST.insert(0)
-BST.insert(4)
-BST.insert(8)
 
-print(BST.search(-1))
+# 0 ~ 999 숫자 중에서 임의로 100개를 추출해서, 이진 탐색 트리에 입력, 검색, 삭제
+import random
+
+# 0 ~ 999 중, 100 개의 숫자 랜덤 선택
+bst_nums = set()
+while len(bst_nums) != 100:
+    bst_nums.add(random.randint(0, 999))
+# print (bst_nums)
+
+# 선택된 100개의 숫자를 이진 탐색 트리에 입력, 임의로 루트노드는 500을 넣기로 함
+head = Node(500)
+binary_tree = NodeMgmt(head)
+for num in bst_nums:
+    binary_tree.insert(num)
+    
+# 입력한 100개의 숫자 검색 (검색 기능 확인)
+for num in bst_nums:
+    if binary_tree.search(num) == False:
+        print ('search failed', num)
+
+# 입력한 100개의 숫자 중 10개의 숫자를 랜덤 선택
+delete_nums = set()
+bst_nums = list(bst_nums)
+while len(delete_nums) != 10:
+    delete_nums.add(bst_nums[random.randint(0, 99)])
+
+# 선택한 10개의 숫자를 삭제 (삭제 기능 확인)
+for del_num in delete_nums:
+    if binary_tree.delete(del_num) == False:
+        print('delete failed', del_num)
