f9b9ef4fad72cdf6d936078e532e25cd5c813f68
momo <wdudtlrw78@gmail.com>
2021-04-03 23:06:51 +0900

    Updated MergeSort

---
+++ data-structure/algorithms/MergeSort.js
@@ -0,0 +1,26 @@
+/** @format */
+
+const mergeSort = function (array) {
+  if (array.length < 2) return array; // 원소가 하나일 때는 그대로 내보냅니다.
+  const pivot = Math.floor(array.length / 2); // 대략 반으로 쪼개는 코드
+  const left = array.slice(0, pivot); // 쪼갠 왼쪽 (0 에서 pivot)
+  const right = array.slice(pivot, array.length); // 쪼갠 오른쪽 ( pivot 부터 array.length)
+  return merge(mergeSort(left), mergeSort(right)); // 재귀적으로 쪼개고 합칩니다.
+};
+
+function merge(left, right) {
+  var result = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      // 두 배열의 첫 원소를 비교하여
+      result.push(left.shift()); // 더 작은 수를 결과에 넣어줍니다.
+    } else {
+      result.push(right.shift()); // 오른쪽도 마찬가지
+    }
+  }
+  while (left.length) result.push(left.shift()); // 어느 한 배열이 더 많이 남았다면 나머지를 다 넣어줍니다.
+  while (right.length) result.push(right.shift()); // 오른쪽도 마찬가지
+  return result;
+}
+
+console.log(mergeSort([5, 2, 4, 7, 6, 1, 3, 8])); // [1, 2, 3, 4, 5, 6, 7, 8]
