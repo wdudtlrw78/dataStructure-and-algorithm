58ce210a2e0ee425e39646a447ad811aa412e647
momo <wdudtlrw78@gmail.com>
2021-04-21 13:33:20 +0900

    알고리즘 기본 풀이 홀수

---
+++ js-algorithmProblem/기본문제 풀이/홀수.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const arr = [12, 77, 38, 41, 53, 92, 85];
+
+function solution(arr) {
+  const answer = [];
+  let sumNumber = 0,
+    min = Number.MAX_SAFE_INTEGER;
+  for (let oddNumber of arr) {
+    if (oddNumber % 2 === 1) {
+      sumNumber += oddNumber;
+      if (oddNumber < min) min = oddNumber;
+    }
+  }
+  answer.push(sumNumber);
+  answer.push(min);
+  return answer;
+}
+
+console.log(solution(arr));
