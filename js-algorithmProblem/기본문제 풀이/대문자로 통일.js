1495ae829cf5a85545a44538eadf618ae89fa87f
momo <wdudtlrw78@gmail.com>
2021-04-22 21:43:00 +0900

    알고리즘 기본 풀이 대문자로 통일

--- js-algorithmProblem/기본문제 풀이/대문자로 통일.js
+++ js-algorithmProblem/기본문제 풀이/대문자로 통일.js
@@ -3,8 +3,33 @@
 const str = 'ItisTimeStudy';
 
 function solution(str) {
+  let answer = '';
+  for (let up of str) {
+    if (up === up.toLowerCase()) answer += up.toUpperCase();
+    else answer += up;
+  }
+  return answer;
+}
+
+function solution(str) {
   const answer = str.toUpperCase();
 
+  return answer;
+}
+
+function solution(str) {
+  let answer = '';
+  for (let up of str) {
+    // 각 문자별 아스키 넘버
+    // 대문자 : 65(A) ~ 90(Z)
+    // 소문자 : 97(a) ~ 122(z)
+    let num = up.charCodeAt();
+
+    // 97 - 32 = 65(A) , 98 - 32 = 66(B) ...
+    // fromCharCode = 해당 아스키 코드 문자열로 변환해준다.
+    if (num >= 97 && num <= 122) answer += String.fromCharCode(num - 32);
+    else answer += up;
+  }
   return answer;
 }
 
