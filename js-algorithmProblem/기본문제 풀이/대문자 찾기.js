faf1c909910fbe0531bcc3caf0445459126966fd
momo <wdudtlrw78@gmail.com>
2021-04-22 21:32:37 +0900

    알고리즘 기본 풀이 대문자 찾기

--- js-algorithmProblem/기본문제 풀이/대문자 찾기.js
+++ js-algorithmProblem/기본문제 풀이/대문자 찾기.js
@@ -3,9 +3,12 @@
 function solution(string) {
   let count = 0;
   for (let x of string) {
-    if (x === x.toUpperCase()) {
-      count++;
-    }
+    // 각 문자별 아스키 넘버
+    // 대문자 : 65(A) ~ 90(Z)
+    // 소문자 : 97(a) ~ 122(z)
+    // let num = x.charCodeAt();
+    // if (num >= 65 && num <= 90) count++;
+    if (x === x.toUpperCase()) count++;
   }
   return count;
 }
