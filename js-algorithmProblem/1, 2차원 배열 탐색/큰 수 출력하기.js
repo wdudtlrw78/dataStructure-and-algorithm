ea291ffd740b1429279d89a04fed28d2bbe90494
momo <wdudtlrw78@gmail.com>
2021-04-26 13:22:48 +0900

    알고리즘 1,2차원 배열 탐색 - 큰 수 출력하기

---
+++ js-algorithmProblem/1, 2차원 배열 탐색/큰 수 출력하기.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const numbers = [7, 3, 9, 5, 6, 12];
+
+function solution(numbers) {
+  let answer = [];
+  answer.push(numbers[0]);
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > numbers[i - 1]) answer.push(numbers[i]);
+  }
+  return answer;
+}
+
+console.log(solution(numbers)); // [7, 9, 6, 12]
