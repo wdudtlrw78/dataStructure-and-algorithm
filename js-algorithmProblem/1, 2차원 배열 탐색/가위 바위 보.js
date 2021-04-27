9a6f6c591b45ba4a93399e6ad3a4a5030f6b7955
momo <wdudtlrw78@gmail.com>
2021-04-27 14:22:18 +0900

    알고리즘 1,2차원 배열 탐색 - 가위 바위 보

---
+++ js-algorithmProblem/1, 2차원 배열 탐색/가위 바위 보.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const a = [2, 3, 3, 1, 3];
+const b = [1, 1, 2, 2, 3];
+
+function solution(a, b) {
+  let answer = '';
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b[i]) answer += 'D' + ' ';
+    else if (a[i] === 1 && b[i] === 3) answer += 'A' + ' ';
+    else if (a[i] === 2 && b[i] === 1) answer += 'A' + ' ';
+    else if (a[i] === 3 && b[i] === 2) answer += 'A' + ' ';
+    else answer += 'B' + ' ';
+  }
+
+  return answer;
+}
+
+console.log(solution(a, b)); // A B A B D
--- test.js
+++ test.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const arr = ['good', 'time', 'good', 'time', 'student'];
+const arr = [7, 3, 9, 5, 6, 12];
 
 function solution(arr) {
-  let answer;
+  let answer = [];
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] < arr[i]) answer.push(arr[i]);
+  }
 
-  answer = arr.filter((v, i) => {
-    if (arr.indexOf(v) === i) return v;
-  });
   return answer;
 }
 
