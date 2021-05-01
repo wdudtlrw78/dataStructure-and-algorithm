5f5d6345c375250c90f310a4c7406c6a01457259
momo <wdudtlrw78@gmail.com>
2021-05-01 21:34:34 +0900

    프로그래머스Lv1 - 나누어 떨어지는 숫자 배열

--- js-algorithmProblem/프로그래머스 Lv1/나누어 떨어지는 숫자 배열.js
+++ js-algorithmProblem/프로그래머스 Lv1/나누어 떨어지는 숫자 배열.js
@@ -1,3 +1,4 @@
+// 내 코드
 function solution(arr, divisor) {
   var answer = [];
   for (let i = 0; i < arr.length; i++) {
@@ -11,6 +12,12 @@ function solution(arr, divisor) {
     answer.push(-1);
   }
   return answer;
+}
+
+// best 코드
+function solution(arr, divisor) {
+  var answer = arr.filter((v) => v % divisor === 0);
+  return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
 }
 console.log(solution([5, 9, 7, 10], 5));
 console.log(solution([2, 36, 1, 3], 1));
