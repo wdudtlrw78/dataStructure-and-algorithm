9180903adb9f8cb78d5618c3965b030d072f018f
momo <wdudtlrw78@gmail.com>
2021-05-02 17:30:05 +0900

    1, 2차원 배열 탐색 - 등수구하기

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
 'use strict';
 
+const arr = [87, 89, 92, 100, 76];
 function solution(arr) {
-  let answer = [];
-  answer.push(arr[0]);
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > arr[i - 1]) answer.push(arr[i]);
+  let n = arr.length;
+  let answer = Array.from({ length: n }, () => 1);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(arr[i] < arr[j]);
+    }
   }
-
   return answer;
 }
 
-console.log(solution([7, 3, 9, 5, 6, 12]));
+console.log(solution(arr));
