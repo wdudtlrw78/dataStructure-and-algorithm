19c83296ba69472e8fc67073320c4654b2b7e6d6
momo <wdudtlrw78@gmail.com>
2021-05-02 18:03:41 +0900

    1,2차원 배열 탐색 - 격자판 최대합

--- js-algorithmProblem/1, 2차원 배열 탐색/격자판 최대합.js
+++ js-algorithmProblem/1, 2차원 배열 탐색/격자판 최대합.js
@@ -1,4 +1,4 @@
-'use strict';
+('use strict');
 
 const arr = [
   [10, 13, 10, 12, 15],
@@ -9,24 +9,33 @@ const arr = [
 ];
 
 function solution(arr) {
-  let answer = Number.MIN_SAFE_INTEGER;
+  let answer = 0;
   let n = arr.length;
-  let sum1 = 0;
-  let sum2 = 0;
+  let horizontal = 0;
+  let vertical = 0;
+  let leftOppositeAngle = 0;
+  let rightOppositeAngle = 0;
+
   for (let i = 0; i < n; i++) {
-    sum1 = sum2 = 0; // 0 초기화 안해버리면 계속 + 누적되니까 j문 전에 초기화
+    horizontal = vertical = 0; // 0 초기화 안해버리면 계속 + 누적되니까 j문 전에 초기화
     for (let j = 0; j < n; j++) {
-      sum1 += arr[i][j]; // 열 [0][0] [0][1] [0][2] ...
-      sum2 += arr[j][i]; // 행 [0][0] [1][0] [2][0] ...
+      horizontal += arr[i][j]; // 열 [0][0] [0][1] [0][2] ...
+      vertical += arr[j][i]; // 행 [0][0] [1][0] [2][0] ...
     }
-    answer = Math.max(answer, sum1, sum2);
+
+    leftOppositeAngle = rightOppositeAngle = 0;
+    for (let i = 0; i < n; i++) {
+      leftOppositeAngle += arr[i][i];
+      rightOppositeAngle += arr[i][n - i - 1];
+    }
+
+    answer = Math.max(
+      horizontal,
+      vertical,
+      leftOppositeAngle,
+      rightOppositeAngle
+    );
   }
-  sum1 = sum2 = 0;
-  for (let i = 0; i < n; i++) {
-    sum1 += arr[i][i];
-    sum2 += arr[i][n - i - 1];
-  }
-  answer = Math.max(answer, sum1, sum2);
   return answer;
 }
 
--- test.js
+++ test.js
@@ -1,13 +1,45 @@
 'use strict';
 
-const arr = [87, 89, 92, 100, 76];
+const arr = [
+  [10, 13, 10, 12, 15],
+  [12, 39, 30, 23, 11],
+  [11, 25, 50, 53, 15],
+  [19, 27, 29, 37, 27],
+  [19, 13, 30, 13, 19],
+];
+
 function solution(arr) {
+  let answer = 0;
   let n = arr.length;
-  let answer = Array.from({ length: n }, () => 1);
+  let horizontal = 0;
+  let vertical = 0;
+  let leftOppositeAngle = 0;
+  let rightOppositeAngle = 0;
+
   for (let i = 0; i < n; i++) {
+    horizontal = vertical = leftOppositeAngle = rightOppositeAngle = 0;
     for (let j = 0; j < n; j++) {
-      console.log(arr[i] < arr[j]);
+      horizontal += arr[i][j];
+      vertical += arr[j][i];
     }
+    answer = Math.max(
+      horizontal,
+      vertical,
+      leftOppositeAngle,
+      rightOppositeAngle
+    );
+
+    horizontal = vertical = leftOppositeAngle = rightOppositeAngle = 0;
+    for (let k = 0; k < n; k++) {
+      leftOppositeAngle += arr[k][k];
+      rightOppositeAngle += arr[k][n - k - 1];
+    }
+    answer = Math.max(
+      horizontal,
+      vertical,
+      leftOppositeAngle,
+      rightOppositeAngle
+    );
   }
   return answer;
 }
