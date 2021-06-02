4f441b0d9c813e123839fbb89f0b50b130b297fc
momo <wdudtlrw78@gmail.com>
2021-06-02 23:46:38 +0900

    test

--- test.js
+++ test.js
@@ -1,34 +1,32 @@
 'use strict';
 
-const arr = [
+const test = [
   [3, 4, 1, 2],
   [4, 3, 2, 1],
   [3, 1, 4, 2],
 ];
 
-function solution(arr) {
+function solution(test) {
   let answer = 0;
-  let n = arr[0].length;
-  let m = arr.length;
+  let n = test[0].length;
+  let m = test.length;
 
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
-      let ctn = 0;
+      let cnt = 0;
       for (let k = 0; k < m; k++) {
-        let pi,
-          pj = 0;
+        let pi, pj;
         for (let s = 0; s < n; s++) {
-          if (arr[k][s] === i) pi = s;
-          if (arr[k][s] === j) pj = s;
+          if (test[k][s] === i) pi = s;
+          if (test[k][s] === j) pj = s;
         }
-        if (pi < pj) ctn++;
+        if (pi < pj) cnt++;
       }
-      if (ctn === m) {
-        answer++;
-      }
+      if (cnt === m) answer++;
     }
   }
   return answer;
 }
 
-console.log(solution(arr));
+console.log(solution(test));
+console.log(solution(test));
