2a979598c260fdcd7afb5eaadde97189ae38e149
momo <wdudtlrw78@gmail.com>
2021-12-02 01:50:27 +0900

    복습: 가운데 문자 출력

--- test.js
+++ test.js
@@ -1,19 +1,14 @@
 'use strict';
 
-const arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+const arr = 'study';
 
 function solution(arr) {
-  const sum = arr.reduce((acc, cur) => acc + cur);
-  const answer = arr;
+  const count = Math.floor(arr.length / 2);
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j < arr.length - 1; j++) {
-      if (sum - (arr[i] + arr[j]) === 100) {
-        answer.splice(j, 1);
-        answer.splice(i, 1);
-      }
-    }
-  }
-  return answer;
+  return arr.length % 2 === 0
+    ? arr
+        // good
+        .substring(count - 1, count + 1)
+    : arr.substring(count, count + 1);
 }
 console.log(solution(arr));
