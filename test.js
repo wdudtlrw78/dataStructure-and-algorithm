3c9aee88350b87d1800e3dd8705ab6425f879ac4
momo <wdudtlrw78@gmail.com>
2021-12-03 00:58:14 +0900

    복습: 큰 수 출력하기

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const arr = ['good', 'time', 'good', 'time', 'student'];
+const arr = [7, 3, 9, 5, 6, 12];
 
 function solution(arr) {
-  const answer = arr;
+  const answer = [];
 
-  return Array.from(new Set(answer));
+  answer.push(arr[0]);
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) answer.push(arr[i]);
+  }
+
+  return answer;
 }
 console.log(solution(arr));
