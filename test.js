50c54baed8afba655bbb44f73208e6fed4762291
momo <wdudtlrw78@gmail.com>
2021-12-09 16:29:35 +0900

    복습: 점수계산

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
 'use strict';
 
-const arr = [130, 135, 148, 140, 145, 150, 150, 153];
+const arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
 
 function solution(arr) {
-  let answer = 1;
-  let max = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (max < arr[i]) {
-      max = arr[i];
-      answer++;
+  let answer = 0;
+  let k = 0;
+
+  for (let x of arr) {
+    if (x === 1) {
+      k++;
+      answer += k;
+    } else {
+      k = 0;
     }
   }
 
