67307a063467bed91a0f7cc48379b583445c69b7
momo <wdudtlrw78@gmail.com>
2021-12-10 20:12:09 +0900

    복습: 격자판 최대합

--- test.js
+++ test.js
@@ -1,17 +1,38 @@
 'use strict';
 
-const arr = [87, 89, 92, 100, 76];
+const arr = [
+  [10, 13, 10, 12, 15],
+  [12, 39, 30, 23, 11],
+  [11, 25, 50, 53, 15],
+  [19, 27, 29, 37, 27],
+  [19, 13, 30, 13, 19],
+];
 
 function solution(arr) {
-  let answer = Array.from({ length: arr.length }, () => 1);
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[i] < arr[j]) {
-        answer[i]++;
-      }
+  let answer = 0;
+  const length = arr.length;
+  let row = 0;
+  let col = 0;
+  let left = 0;
+  let right = 0;
+  for (let i = 0; i < length; i++) {
+    row = 0;
+    col = 0;
+    for (let j = 0; j < length; j++) {
+      row += arr[j][i];
+      col += arr[i][j];
     }
+
+    answer = Math.max(row, col);
   }
+
+  for (let i = 0; i < length; i++) {
+    left += arr[i][i];
+    right += arr[i][length - i - 1];
+
+    answer = Math.max(left, right);
+  }
+
   return answer;
 }
 console.log(solution(arr));
