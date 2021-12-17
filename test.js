2c965b7d852e1c01f872142f82fcaa49d930a4c5
momo <wdudtlrw78@gmail.com>
2021-12-18 00:25:43 +0900

    복습: 유효한 팰린드롬

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 'use strict';
 
-const arr = 'gooG';
+const arr = 'found7, time: study; Yduts; emit, 7Dnuof';
 
 function solution(arr) {
   let answer = 'YES';
-  const text = arr.toUpperCase();
+  const text = arr.toLowerCase();
+  const re = /[A-Z]/gi;
+  const match = text.match(re).join('');
+  const len = match.length;
 
-  for (let i = 0; i < text.length / 2; i++) {
-    text[i] === text[text.length - i - 1] ? answer : (answer = 'NO');
+  for (let i = 0; i < len / 2; i++) {
+    match[i] === match[len - i - 1] ? answer : (answer = 'NO');
   }
+
   return answer;
 }
 console.log(solution(arr));
